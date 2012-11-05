070d53cfe37b25e4477f02f042aaddd578543d0e
Matthew Gerstman <mattgerstman@gmail.com>
2012-11-05 15:49:34 -0500

    started adding notice

--- closed.php
+++ closed.php
@@ -1,4 +1,31 @@
 <?php
+/* Dance Marathon Captain Application
+    Copyright 2012 Dance Marathon at the University of Florida
+
+This product includes software developed by the Dance Marathon at the University of Florida 2013 Technology Team.
+The following developers contributed to this project:
+	Matthew Gerstman
+	Eric Smith
+
+Dance Marathon at the University of Florida is a year-long effort that culminates in a 26.2-hour event where over 800 students stay awake and on their feet to symbolize the obstacles faced by children with serious illnesses or injuries. The event raises money for Shands Hospital for Children, your local Children’s Miracle Network Hospital, in Gainesville, FL. Our contributions are used where they are needed the most, including, but not limitied to, purchasing life-saving medial equipment, funding pediatric research, and purchasing diversionary activities for the kids.
+
+For more information you can visit http://floridadm.org
+   
+This software includes the following open source plugins listed below:
+	Title:		HTML5 Image Uploader
+	Link:		http://tutorialzine.com/2011/09/html5-file-upload-jquery-php/
+	Copyright: 	None, but we're nice and want to give credit.
+
+	Title:		jQuery Validation Plugin 1.9.0
+	Link:		http://bassistance.de/jquery-plugins/jquery-plugin-validation/
+	Copyright:	Copyright (c) 2006 - 2011 Jörn Zaefferer
+	License:	MIT License (http://www.opensource.org/licenses/mit-license.php)
+
+	Title:		TableSorter 2.0 - Client-side table sorting with ease!
+	Link:		http://tablesorter.com
+	Copyright:	Copyright (c) 2007 Christian Bach
+	License:	http://www.opensource.org/licenses/mit-license.php */
+	
 function ae_detect_ie()
 {
     if (isset($_SERVER['HTTP_USER_AGENT']) && 
